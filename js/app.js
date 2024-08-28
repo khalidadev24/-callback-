5cfb8137a8b54e6a0212a363c285874f6703ac85
khalidanejrabi <khalidanejra40@gmail.com>
2024-08-28 15:52:55 -0400

    add simple example of callbak function

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+function greet(name){
+    console.log(`hello ${name}`)
+    
+}
+
+
+// greet ('jhon doe)
+const hof = (callback) =>{
+  const username = prompt('please enter your name?')  
+}
+
+
+hof (greet)// greet is a callback function 
